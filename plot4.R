67354fdb32e63ab052cf0a9a2d146ef1bd130a00
Marko Mitic <marko.mitic@gmail.com>
2014-10-13 01:20:39 +0200

    Decreasing size of the legend

--- plot4.R
+++ plot4.R
@@ -15,7 +15,7 @@ with(data, {
     plot(Sub_metering_1~Datetime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
     lines(Sub_metering_2~Datetime,col='Red')
     lines(Sub_metering_3~Datetime,col='Blue')
-    legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+    legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), cex=0.6)
     plot(Global_reactive_power~Datetime, type="l", ylab="Global Rective Power (kilowatts)",xlab="")
 })
 
